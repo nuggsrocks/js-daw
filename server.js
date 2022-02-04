53b452046b7e452f9f39a289b6a042cc96daaa56
mattfisher <nuggsrocks@yahoo.com>
2022-02-04 01:06:12 -0500

    integrate express server with webpack

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
-const express = require('express')
 const path = require('path')
+const express = require('express')
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackConfig = require('./webpack.config')
+const compiler = webpack(webpackConfig)
 
 const app = express()
-
-app.use(express.static(path.resolve(__dirname, '/public')))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  res.send('Hello world!')
-})
+const NODE_ENV = process.env.NODE_ENV
 
-app.use((req, res) => {
-  res.send('404 Not Found')
-})
+if (typeof NODE_ENV === 'undefined' || NODE_ENV === 'development') {
+  app.use(webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath
+  }))
+} else {
+  app.use(express.static(path.resolve(__dirname, '/public')))
+
+
+  app.get('/', (req, res) => {
+    res.send('Hello world!')
+  })
+
+  app.use((req, res) => {
+    res.send('404 Not Found')
+  })
+}
+
 
 const PORT = process.env.PORT || 8080
 const HOST = process.env.HOST || 'localhost'
