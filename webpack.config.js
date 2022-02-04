54e3941ecca32b672147bfc0629ad9fb2badbdda
mattfisher <nuggsrocks@yahoo.com>
2022-02-04 00:52:01 -0500

    webpack setup and config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval',
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader'
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+}
